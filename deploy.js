dc56ca0ef632337fd03b1c915047511fc639055d
richie48 <57833496+richie48@users.noreply.github.com>
2022-09-11 15:09:38 +0100

    deployed contract to local blockchain

--- deploy.js
+++ deploy.js
@@ -1,3 +1,23 @@
-function hi() {
-  console.log("hi");
+const ethers = require("ethers");
+const fs = require("fs");
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider("http://0.0.0.0:8545");
+  const wallet = new ethers.Wallet(
+    "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+    provider
+  );
+
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf-8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("deploying...");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
+
+main();
